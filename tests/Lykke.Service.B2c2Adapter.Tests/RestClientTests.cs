50da2e8aef2e8a7e7574c4d93322e3fd9a131ad6
Alexey Zelenin <alexey@zelenin.org>
2018-09-18 11:50:29 +0300

    Commented test that can work local only

--- tests/Lykke.Service.B2c2Adapter.Tests/RestClientTests.cs
+++ tests/Lykke.Service.B2c2Adapter.Tests/RestClientTests.cs
@@ -6,8 +6,8 @@ namespace Lykke.Service.B2c2Adapter.Tests
 {
     public class RestClientTests
     {
-        [Fact]
-        public async void Test()
+        //[Fact]
+        public async void BalanceTest()
         {
             var restClient = new B2C2RestClient("1bbe66d9dda462f37e7159c091e86994593b88d5", LogFactory.Create());
 
