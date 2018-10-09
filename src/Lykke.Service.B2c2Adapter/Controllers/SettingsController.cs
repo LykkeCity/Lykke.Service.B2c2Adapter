a908d53a525e2c4267d393b85deaaa29cb2d1935
Alexey Zelenin <alexey@zelenin.org>
2018-10-09 22:46:11 +0300

    Added GetSettings() to API

---
+++ src/Lykke.Service.B2c2Adapter/Controllers/SettingsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Lykke.Service.B2c2Adapter.Services;
+using Lykke.Service.B2c2Adapter.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Lykke.Service.B2c2Adapter.Controllers
+{
+    [Route("/api/[controller]")]
+    public sealed class SettingsController
+    {
+        private readonly OrderBooksService _orderBooksService;
+
+        public SettingsController(OrderBooksService orderBooksService)
+        {
+            _orderBooksService = orderBooksService;
+        }
+
+        [SwaggerOperation("GetSettings")]
+        [HttpGet]
+        [ProducesResponseType(typeof(OrderBooksServiceSettings), (int)HttpStatusCode.OK)]
+        public OrderBooksServiceSettings Get()
+        {
+            return _orderBooksService.GetSettings();
+        }
+    }
+}
