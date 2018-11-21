c2a26553e6b6d43c2ca1a4402e2719ba73a432e3
Alexey Zelenin <alexey@zelenin.org>
2018-11-21 10:15:16 +0300

    Fixed reloading trades history

--- src/Lykke.Service.B2c2Adapter/Services/TradeHistoryService.cs
+++ src/Lykke.Service.B2c2Adapter/Services/TradeHistoryService.cs
@@ -45,7 +45,9 @@ namespace Lykke.Service.B2c2Adapter.Services
                     var offset = 0;
                     var data = await _b2C2RestClient.GetTradeHistoryAsync(offset, 100);
 
-                    await context.Database.ExecuteSqlCommandAsync($"TRUNCATE TABLE {Constants.Schema}.{Constants.TradesTable}");
+                    var query = $"TRUNCATE TABLE {Constants.Schema}.{Constants.TradesTable}";
+
+                    await context.Database.ExecuteSqlCommandAsync(query);
 
                     while (data.Any())
                     {
