3eb1bdcf1d5940ce25bd4bbfda54a304749af50e
Alexey Zelenin <alexey@zelenin.org>
2018-11-21 09:53:13 +0300

    Added reconnect if fail connecting to WS on start

--- src/Lykke.Service.B2c2Adapter/Services/TradeHistoryService.cs
+++ src/Lykke.Service.B2c2Adapter/Services/TradeHistoryService.cs
@@ -60,16 +60,10 @@ namespace Lykke.Service.B2c2Adapter.Services
                     return offset;
                 }
             }
-            catch (Exception e)
-            {
-                _log.Warning($"Exception while reloading trade history: {e}.");
-            }
             finally
             {
                 StopWork();
             }
-
-            return -1;
         }
 
         private ReportContext CreateContext()
@@ -110,10 +104,6 @@ namespace Lykke.Service.B2c2Adapter.Services
                         data = await _b2C2RestClient.GetTradeHistoryAsync(offset, 10, ct);
                     } while (added > 0);
                 }
-            }
-            catch (Exception e)
-            {
-                _log.Warning($"Exception while getting trading history and writing it to the database: {e}.");
             }
             finally 
             {
