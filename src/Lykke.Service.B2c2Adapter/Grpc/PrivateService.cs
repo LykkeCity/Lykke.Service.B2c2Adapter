44dea28a49825d0c1c875475f03f7eb910309af3
Dmitry Samodov <dmitry.samodov@swisschain.io>
2021-04-22 10:51:46 +0300

    XMM-590: updated liquidity.trading contract

--- src/Lykke.Service.B2c2Adapter/Grpc/PrivateService.cs
+++ src/Lykke.Service.B2c2Adapter/Grpc/PrivateService.cs
@@ -8,6 +8,7 @@ using Grpc.Core;
 using Lykke.B2c2Client;
 using Lykke.B2c2Client.Models.Rest;
 using Swisschain.Liquidity.ApiContract;
+using Trade = Swisschain.Liquidity.ApiContract.Trade;
 
 namespace Lykke.Service.B2c2Adapter.Grpc
 {
@@ -29,7 +30,7 @@ namespace Lykke.Service.B2c2Adapter.Grpc
             return result;
         }
 
-        public override async Task<MarketOrderResponse> MarketOrder(MarketOrderRequest request, ServerCallContext context)
+        public override async Task<ExecuteMarketOrderResponse> ExecuteMarketOrder(MarketOrderRequest request, ServerCallContext context)
         {
             var orderId = Guid.NewGuid().ToString();
             decimal size = decimal.Parse(request.Size, CultureInfo.InvariantCulture);
@@ -46,16 +47,56 @@ namespace Lykke.Service.B2c2Adapter.Grpc
                 ValidUntil = DateTime.UtcNow.AddSeconds(3)
             });
 
-            return new MarketOrderResponse
+            var result = new ExecuteMarketOrderResponse
             {
                 OrderId = response.OrderId,
-                TradeId = response.Trades.Single().TradeId,
-                RequestId = orderId,
+                RequestId = response.ClientOrderId,
                 AssetPairId = request.AssetPair,
                 Price = response.ExecutedPrice.ToString(CultureInfo.InvariantCulture),
-                Size = response.Quantity.ToString(CultureInfo.InvariantCulture),
+                FilledSize = (response.Side == Side.Buy ? response.Quantity : -response.Quantity).ToString(CultureInfo.InvariantCulture),
+                CancelledSize = "0",
                 Timestamp = response.Created.ToTimestamp(),
                 Error = ErrorCore.Ok
+            };
+
+            result.Trades.AddRange(MapTrades(response.Trades));
+
+            return result;
+        }
+
+        private List<Trade> MapTrades(IReadOnlyCollection<B2c2Client.Models.Rest.Trade> trades)
+        {
+            return trades.Select(x => new Trade
+            {
+                Id = x.TradeId,
+                Price = x.Price.ToString(CultureInfo.InvariantCulture),
+                FilledSize = (x.Side == Side.Buy ? x.Quantity : -x.Quantity).ToString(CultureInfo.InvariantCulture),
+                CancelledSize = "0",
+                Timestamp = x.Created.ToTimestamp()
+            }).ToList();
+        }
+
+        public override async Task<PlaceMarketOrderResponse> PlaceMarketOrder(MarketOrderRequest request, ServerCallContext context)
+        {
+            var orderId = Guid.NewGuid().ToString();
+            decimal size = decimal.Parse(request.Size, CultureInfo.InvariantCulture);
+
+            var response = await _b2C2RestClient.OrderAsync(new OrderRequest
+            {
+                ClientOrderId = orderId,
+                Instrument = request.AssetPair,
+                Side = size > 0
+                    ? Side.Buy
+                    : Side.Sell,
+                Quantity = Math.Abs(size),
+                OrderType = OrderType.MKT,
+                ValidUntil = DateTime.UtcNow.AddSeconds(3)
+            });
+
+            return new PlaceMarketOrderResponse
+            {
+                Error = ErrorCore.Ok,
+                RequestId = orderId
             };
         }
 
